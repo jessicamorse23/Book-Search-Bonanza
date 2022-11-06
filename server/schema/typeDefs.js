1f13ac629dae9a57ee2a5c04dfc536e5be24384c
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-11-06 17:24:37 -0500

    works in sandbox - does not render.

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -37,7 +37,7 @@ type Query {
 
 type Mutation {
   login(email: String!, password: String!): Auth
-  addUser(username: String!, email: STring!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
   saveBook(input: BookInput): User
   removeBook(bookId: String!): User
 }
